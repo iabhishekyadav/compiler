65055d6f4cd87c77a8248d83d77796cd7abde927
Abhishek Yadav <abhishek1.yadav@paytm.com>
2017-04-13 18:45:56 +0530

    added basic model

---
+++ model/problem.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var db = require('/config/db');
+var problem = new db({tableName: "problem"});
+
+
+var model = {
+
+	get: function(options , callback){
+		var data = null;
+		if(options.id){
+
+		}
+	}
+};
